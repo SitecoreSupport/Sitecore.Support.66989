2cc96b045c407c2d1a4d80c6f5bcdafe36e5c1c8
DK\NPA <npa@sitecore.net>
2018-02-01 16:33:50 +0200

    Native code

--- src/Sitecore.Support.66989/Forms/Core/Commands/SetSubmitActions.cs
+++ src/Sitecore.Support.66989/Forms/Core/Commands/SetSubmitActions.cs
@@ -1,75 +1,74 @@
 ﻿using Sitecore.Data;
 using Sitecore.Data.Items;
 using Sitecore.Diagnostics;
+using Sitecore.Form.Core.Configuration;
 using Sitecore.Form.Core.ContentEditor.Data;
+using Sitecore.Forms.Core.Commands;
 using Sitecore.Forms.Core.Data;
 using Sitecore.Globalization;
 using Sitecore.Text;
 using Sitecore.Web;
 using Sitecore.Web.UI.Sheer;
-using Sitecore.WFFM.Abstractions.Dependencies;
+using Sitecore.WFFM.Core.Resources;
 using System;
 using System.Web;
-
 namespace Sitecore.Support.Forms.Core.Commands
 {
-  [Serializable]
-  public class SetSubmitActions : Sitecore.Forms.Core.Commands.SetSubmitActions
-  {
-    protected new void Run(ClientPipelineArgs args)
+    [Serializable]
+    public class SetSubmitActions : Sitecore.Forms.Core.Commands.SetSubmitActions
     {
-      Assert.ArgumentNotNull(args.Parameters["id"], "id");
-      Assert.ArgumentNotNull(args.Parameters["db"], "db");
-      ItemUri uri = new ItemUri(ID.Parse(args.Parameters["id"]), Language.Parse(args.Parameters["la"]), Data.Version.Latest, args.Parameters["db"]);
-      Item innerItem = Database.GetItem(uri);
-      if (SheerResponse.CheckModified() && (innerItem != null))
-      {
-        if (args.IsPostBack)
+        protected new void Run(ClientPipelineArgs args)
         {
-          UrlString urlString = new UrlString(args.Parameters["url"]);
-          UrlHandle handle = UrlHandle.Get(urlString);
-          innerItem.Editing.BeginEdit();
-          innerItem.Fields["__Tracking"].Value = handle["tracking"];
-          if (args.HasResult)
-          {
-            ListDefinition definition = ListDefinition.Parse((args.Result == "-") ? string.Empty : args.Result);
-            if (args.Parameters["mode"] == "save")
+            Assert.ArgumentNotNull(args.Parameters["id"], "id");
+            Assert.ArgumentNotNull(args.Parameters["db"], "db");
+            ItemUri uri = new ItemUri(ID.Parse(args.Parameters["id"]), Language.Current, Sitecore.Data.Version.Latest, args.Parameters["db"]);
+            Item item = Database.GetItem(uri);
+            if (SheerResponse.CheckModified() && item != null)
             {
-              innerItem.Fields[Sitecore.Form.Core.Configuration.FieldIDs.SaveActionsID].Value = definition.ToXml();
+                if (args.IsPostBack)
+                {
+                    UrlString urlString = new UrlString(args.Parameters["url"]);
+                    UrlHandle urlHandle = UrlHandle.Get(urlString);
+                    item.Editing.BeginEdit();
+                    item.Fields["__Tracking"].Value = urlHandle["tracking"];
+                    if (args.HasResult)
+                    {
+                        ListDefinition listDefinition = ListDefinition.Parse((args.Result == "-") ? string.Empty : args.Result);
+                        if (args.Parameters["mode"] == "save")
+                        {
+                            item.Fields[Sitecore.Form.Core.Configuration.FieldIDs.SaveActionsID].Value = listDefinition.ToXml();
+                        }
+                        else
+                        {
+                            item.Fields[Sitecore.Form.Core.Configuration.FieldIDs.CheckActionsID].Value = listDefinition.ToXml();
+                        }
+                    }
+                    item.Editing.EndEdit();
+                    return;
+                }
+                string text = ID.NewID.ToString();
+                UrlString urlString2 = new UrlString(UIUtil.GetUri("control:SubmitCommands.Editor"));
+                FormItem formItem = new FormItem(item);
+                ListDefinition value = ListDefinition.Parse((args.Parameters["mode"] == "save") ? formItem.SaveActions : formItem.CheckActions);
+                System.Web.HttpContext.Current.Session.Add(text, value);
+                urlString2.Append("definition", text);
+                urlString2.Add("id", args.Parameters["id"]);
+                urlString2.Add("db", args.Parameters["db"]);
+                urlString2.Add("la", args.Parameters["la"]);
+                urlString2.Append("root", args.Parameters["root"]);
+                urlString2.Append("system", args.Parameters["system"] ?? string.Empty);
+                args.Parameters.Add("params", text);
+                UrlHandle urlHandle2 = new UrlHandle();
+                urlHandle2["title"] = ResourceManager.Localize((args.Parameters["mode"] == "save") ? "SELECT_SAVE_TITLE" : "SELECT_CHECK_TITLE");
+                urlHandle2["desc"] = ResourceManager.Localize((args.Parameters["mode"] == "save") ? "SELECT_SAVE_DESC" : "SELECT_CHECK_DESC");
+                urlHandle2["actions"] = ResourceManager.Localize((args.Parameters["mode"] == "save") ? "SAVE_ACTIONS" : "CHECK_ACTIONS");
+                urlHandle2["addedactions"] = ResourceManager.Localize((args.Parameters["mode"] == "save") ? "ADDED_SAVE_ACTIONS" : "ADDED_CHECK_ACTIONS");
+                urlHandle2["tracking"] = formItem.Tracking.ToString();
+                urlHandle2.Add(urlString2);
+                args.Parameters["url"] = urlString2.ToString();
+                Context.ClientPage.ClientResponse.ShowModalDialog(urlString2.ToString(), true);
+                args.WaitForPostBack();
             }
-            else
-            {
-              innerItem.Fields[Sitecore.Form.Core.Configuration.FieldIDs.CheckActionsID].Value = definition.ToXml();
-            }
-          }
-          innerItem.Editing.EndEdit();
         }
-        else
-        {
-          string name = ID.NewID.ToString();
-          UrlString str3 = new UrlString(UIUtil.GetUri("control:SubmitCommands.Editor"));
-          FormItem item2 = new FormItem(innerItem);
-          ListDefinition definition2 = ListDefinition.Parse((args.Parameters["mode"] == "save") ? item2.SaveActions : item2.CheckActions);
-          HttpContext.Current.Session.Add(name, definition2);
-          str3.Append("definition", name);
-          str3.Add("id", args.Parameters["id"]);
-          str3.Add("db", args.Parameters["db"]);
-          str3.Add("la", args.Parameters["la"]);
-          str3.Append("root", args.Parameters["root"]);
-          str3.Append("system", args.Parameters["system"] ?? string.Empty);
-          args.Parameters.Add("params", name);
-          UrlHandle handle2 = new UrlHandle();
-          handle2["title"] = DependenciesManager.ResourceManager.Localize((args.Parameters["mode"] == "save") ? "SELECT_SAVE_TITLE" : "SELECT_CHECK_TITLE");
-          handle2["desc"] = DependenciesManager.ResourceManager.Localize((args.Parameters["mode"] == "save") ? "SELECT_SAVE_DESC" : "SELECT_CHECK_DESC");
-          handle2["actions"] = DependenciesManager.ResourceManager.Localize((args.Parameters["mode"] == "save") ? "SAVE_ACTIONS" : "CHECK_ACTIONS");
-          handle2["addedactions"] = DependenciesManager.ResourceManager.Localize((args.Parameters["mode"] == "save") ? "ADDED_SAVE_ACTIONS" : "ADDED_CHECK_ACTIONS");
-          handle2["tracking"] = item2.Tracking.ToString();
-          handle2.Add(str3);
-          args.Parameters["url"] = str3.ToString();
-          Context.ClientPage.ClientResponse.ShowModalDialog(str3.ToString(), true);
-          args.WaitForPostBack();
-        }
-      }
     }
-  }
 }
